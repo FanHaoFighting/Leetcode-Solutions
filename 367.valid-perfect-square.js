11cb4629003891f3671dba137c949e38bc6f891b
fanhao <fanhaohf@163.com>
2019-06-24 21:29:35 +0800

    isPerfectSquare第一次通过

--- 367.valid-perfect-square.js
+++ 367.valid-perfect-square.js
@@ -7,10 +7,15 @@ var isPerfectSquare = function(num) {
     return true
   }
   let res = num
-  // 循环计算到出现小数位置, while内用>=是因为3这个特殊情况
-  while (res * res - x >= 1) {
-    res = res - (res * res - x) / (2 * res)
+  // 循环计算到sqrt(num)的整数部分出现
+  while (res * res - num >= 1) {
+    res = res - (res * res - num) / (2 * res)
   }
-  return Math.floor(res)
+  res = Math.floor(res)
+  if (res * res == num) {
+    return true
+  } else {
+    return false
+  }
 };
 
---
+++ 387.first-unique-character-in-a-string.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    
+};
+
