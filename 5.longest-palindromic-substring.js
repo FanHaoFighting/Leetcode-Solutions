ce38c45a74f7f89db0722256eb669e399c90fdea
fanhao <fanhaohf@163.com>
2019-06-24 20:03:56 +0800

    longestPalindrome换个可读性更强的方法

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -2,35 +2,34 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
+var longestPalindrome = function (s) {
   let res = ""
-  let max = 0
-  for (let i = 0; i <= 2 * (s.length - 1); i ++) {
-    let str = extendToLeftAndRigght(s, i)
-    res = max > str.length ? res : str
-    max = max > str.length ? max : str.length
+  if (s.length == 1) {
+    return s;
+  }
+  for (let i = 0; i < s.length; i++) {
+    let s1 = extendPalindrome(s, i, i)
+    let s2 = extendPalindrome(s, i, i + 1)
+    if (s1.length > res.length) {
+      res = s1;
+    }
+    if (s2.length > res.length) {
+      res = s2;
+    }
   }
   return res
 };
 
-var extendToLeftAndRigght = function(s, index) {
-  let res
-  let left
-  let right
-  if (index % 2 == 0) {
-    res = s.charAt(index / 2)
-    left = index/2 - 1
-    right = index/2 + 1
-  } else {
-    res = ""
-    left = (index - 1) / 2
-    right = (index + 1) / 2
+var extendPalindrome = function (s, left, right) {
+  if (s.charAt(left) != s.charAt(right)) {
+    return ""
   }
-  while (left >= 0 && right <= s.length -1  && s.charAt(left) === s.charAt(right)) {
-    res = s.charAt(left) + res + s.charAt(right)
-    left--
-    right++
+  for (left, right; 0 <= left && right < s.length; left-- , right++) {
+    if (s.charAt(left) != s.charAt(right)) {
+      break;
+    }
   }
-  return res
+  // 左边索引和右边索引的位置分别往反方向移动1
+  return s.substring(left + 1, right);
 }
 
