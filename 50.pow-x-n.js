1bf3364158bd87a21c0ed10c27352fa61d3dc75b
fanhao <fanhaohf@163.com>
2019-06-25 10:09:49 +0800

    50. Pow(x, n)初次提交

--- 50.pow-x-n.js
+++ 50.pow-x-n.js
@@ -7,13 +7,18 @@ var myPow = function (x, n) {
   if (n == 0) {
     return 1
   }
-  if ( n == 1) {
+  if (n == 1) {
     return x
-  } 
-  if (n == -1) {
-    return 1/x
   }
-  let temp = myPow(x, Math.floor(n / 2))
+  if (n == -1) {
+    return 1 / x
+  }
+  let temp
+  if (n > 0) {
+    temp = myPow(x, Math.floor(n / 2))
+  } else  {
+    temp = myPow(x, Math.ceil(n / 2))
+  }
   if (n % 2 == 0) {
     return temp * temp;
   } else {
