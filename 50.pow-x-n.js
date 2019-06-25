a8902f8e3f14bdac22531758cc0e578417d02450
fanhao <fanhaohf@163.com>
2019-06-25 20:51:23 +0800

    50. Pow(x, n)从左往右累计相乘的方法

--- 50.pow-x-n.js
+++ 50.pow-x-n.js
@@ -4,9 +4,21 @@
  * @return {number}
  */
 var myPow = function(x, n) {
+  let positive
+  if (n >= 0) {
+    positive = true
+  } else {
+    positive = false
+  }
+  // 纯粹为了处理testcase: '2.00000\n-2147483648', 防止overflow
+  if (n < 0) {
+    n = -(n + 1)
+  }
+
   var digitWidth = Math.floor(Math.log2(n))
   var t = 1
   
+  // 将n转换为2进制表示, 位数为1的
   while (digitWidth >= 0) {
       var d = (n >> digitWidth) % 2
       if (d == 1) {
@@ -16,8 +28,11 @@ var myPow = function(x, n) {
       }
       digitWidth--
   }
-  
-  return t
+  if (positive) {
+    return t
+  } else {
+    return 1 / t / x
+  }
 };
 
 
