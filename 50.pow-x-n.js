17629d9ca58aad9c3ca06dfa3e0e912925bdf52a
fanhao <fanhaohf@163.com>
2019-06-25 09:14:27 +0800

    isPowerOfTwo第一次通过

---
+++ 50.pow-x-n.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+  if (n == 0) {
+    return 1
+  }
+  if ( n == 1) {
+    return x
+  } 
+  if (n == -1) {
+    return 1/x
+  }
+  let temp = myPow(x, Math.floor(n / 2))
+  if (n % 2 == 0) {
+    return temp * temp;
+  } else {
+    if (n > 0) {
+      return x * temp * temp
+    } else {
+      return (temp * temp) / x
+    }
+  }
+};
+
