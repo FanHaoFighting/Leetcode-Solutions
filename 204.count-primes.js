65e37fe255883f495633a20f916fae4c93277e89
fanhao <fanhaohf@163.com>
2019-06-26 17:25:58 +0800

    pow(x,n)的优化与若干新题目的提交

---
+++ 204.count-primes.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    
+};
+
--- 50.pow-x-n.js
+++ 50.pow-x-n.js
@@ -4,20 +4,12 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-  let positive
-  if (n >= 0) {
-    positive = true
-  } else {
-    positive = false
-  }
-  // 纯粹为了处理testcase: '2.00000\n-2147483648', 防止overflow
-  if (n < 0) {
-    n = -(n + 1)
-  }
-
+  let positive = n >= 0
+ 
   var res = 1
   var temp = x
 
+  n = Math.abs(n)
   while (n > 0) {
       // 当该位为1的时候, temp的值要乘上
       if (n % 2 == 1) {
@@ -25,12 +17,12 @@ var myPow = function(x, n) {
       }
       // temp依次为x^1 x^2 x^4 x^16 x^32....
       temp = temp * temp
-      n = n >> 1
+      n = n >>> 1
   }
   if (positive) {
     return res
   } else {
-    return 1 / res / x
+    return 1 / res
   }
 };
 
